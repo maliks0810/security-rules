c447384ba7c93e9913376f0dc1cf9cee24123ecb
maliks <sumit.malik@tcw.com>
2026-05-16 15:05:16 -0700

    added sql commits

--- TEST_INSERT_SECURITY_EXCEPTION.sql
+++ TEST_INSERT_SECURITY_EXCEPTION.sql
@@ -1,9 +1,17 @@
--- Snowflake test invocation of INSERT_SECURITY_EXCEPTION.
--- Inserts a row for rule 137 against asset 38384LJ83.
+
+--SELECT * FROM 
+--SELECT * FROM TCW_CORE_DEV.DATA_QUALITY.SECURITY_EXCEPTION WHERE ASSET_ID = '38384LJ83'
+--SELECT COALESCE(MAX("SECURITY_EXCEPTION_ID"), 0) FROM SECURITY_EXCEPTION
+--SELECT * FROM RULE
+--SELECT CURRENT_TIMESTAMP()
+--38384LJ83
+
+
+
 
 CALL INSERT_SECURITY_EXCEPTION(
-    900300,                          -- SECURITY_EXCEPTION_ID
-    137,                             -- RULE_ID
+    20133,                          -- SECURITY_EXCEPTION_ID
+    101,                             -- RULE_ID
     CURRENT_TIMESTAMP(),             -- RUN_DATE
     CURRENT_TIMESTAMP(),             -- RUN_START
     NULL,                            -- RUN_END
@@ -16,7 +24,7 @@ CALL INSERT_SECURITY_EXCEPTION(
     NULL,                            -- ASSIGN_TO_DATE
     NULL,                            -- RESOLVE_DATE
     NULL,                            -- BUS_TERM_SOURCE_ID
-    'Cusip Unaudited',               -- ISSUE_DESCRIPTION
+    'Test Exception 12',               -- ISSUE_DESCRIPTION
     NULL,                            -- SOURCE_SYSTEM_CODE
     CURRENT_TIMESTAMP(),             -- CREATED_DATE
     'demo',                          -- CREATED_BY
@@ -32,3 +40,46 @@ CALL INSERT_SECURITY_EXCEPTION(
     NULL,                            -- COMMENTS
     '38384LJ83'                      -- ALADDIN_ID
 );
+
+select 124.0/558.0
+select (631.97 - 723.87) / 631.97
+
+
+SELECT 
+S.AS_OF_DATE
+,BH.ASOFDATEKEY
+,CREATED_DATE
+,S.SECURITY_GROUP_TYPE
+,S.ALADDIN_ID
+,S.ID_BB_GLOBAL
+,CASE WHEN S.HELD_STATUS IN (1,3) THEN 'HELD'
+WHEN S.HELD_STATUS = 2 THEN 'BENCHMARK'
+ELSE 'NOT_HELD' END HELD_STATUS_DESC
+,S.BCLASS_LEVEL1
+,S.BCLASS_LEVEL2
+,S.BCLASS_LEVEL3
+,S.BCLASS_LEVEL4
+,BH.CLASSIFICATION_LEVEL_1_NAME
+,BH.CLASSIFICATION_LEVEL_2_NAME
+,BH.CLASSIFICATION_LEVEL_3_NAME
+,BH.CLASSIFICATION_LEVEL_4_NAME
+
+FROM TCW_CORE.TDC.SECURITY_VW S 
+JOIN TCW_CORE.COMMON.BBG_DATA_LICENSE_SECURITY_HIST_VW BH ON S.ID_BB_GLOBAL = BH.ID_BB_GLOBAL AND S.AS_OF_DATE_KEY = BH.ASOFDATEKEY
+
+WHERE S.ALADDIN_ID = 'Z96LJ05G1'
+
+
+SELECT SECTOR_TITLE, BREAKDOWN_TAG, AS_OF_DATE FROM TCW_ALADDIN.INVESTMENTS.SECURITY_BREAKDOWN WHERE ALADDIN_ID = 'Z96LJ05G1' AND BREAKDOWN_TAG = 'TCWBCLASS' ORDER BY AS_OF_DATE DESC
+
+
+select dfs.field_label, dfs.list_name, dfs.list_type, dft.field_type_name
+from dim_field_specification dfs
+join dim_field_type dft on dft.field_type_id = dfs.field_type_id
+where dfs.field_type_id = 2
+;
+
+AS_OF_DATE_KEY
+LIST_NAME
+LIST_DESCRIPTION
+LIST_TYPE
