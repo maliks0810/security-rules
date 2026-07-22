1a0fe33151f2ac6e054e132431729ab0c1492897
maliks <sumit.malik@tcw.com>
2026-07-22 13:00:44 -0700

    made fixes

---
+++ GithubSrc.cmd
@@ -0,0 +1,2 @@
+git remote remove origin
+git remote add origin https://github.com/maliks0810/security-rules.git
---
+++ GitlabSrc.cmd
@@ -0,0 +1,2 @@
+git remote remove origin
+git remote add origin https://gitlab.com/tcw-group/technology/data-engineering/tcw-data-api/data-quality/security-rules.git
